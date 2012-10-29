705a3d96659bd1e751aa63c6673a0eadc7e94a43
destinyd <destinyd_war@163.com>
2012-10-29 14:31:45 +0800

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Blog::Application.routes.draw do
-  constraints :subdomain => "" do
-    #resources :posts
-    get "blog/archives" => "posts#archives",as: :blog_archives
-    match "/blog/:year/:month/:day/:id" => 'posts#show'
-    match "/blog/categories/:id" => 'posts#tag'
-    root :to => "home#index"
-  end
-
-  constraints :subdomain => "admin" do
+  constraints :subdomain => "admin",as: :admin do
     #namespace :admin do
       resources :posts, :module => "admin"
     #end
     root :to => "admin/posts#index"
+  end
+
+  constraints :subdomain => '' do
+    #resources :posts
+    get "blog/archives" => "posts#archives",as: :blog_archives
+    match "/blog/:year/:month/:day/:id" => 'posts#show'
+    match "/blog/categories/:id" => 'posts#tag'
+    root :to => "home#index",as: :home
   end
 end
